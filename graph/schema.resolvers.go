8c1f965cbb4b1e936990d6ef8ec94e0285cc0819
revaldimijaya <62485151+revaldimijaya@users.noreply.github.com>
2020-07-23 02:36:20 +0700

    asd

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -327,7 +327,7 @@ func (r *queryResolver) GetNextVideo(ctx context.Context, videoid int) ([]*model
 func (r *queryResolver) GetVideoLike(ctx context.Context, videoid int, typeArg string) ([]*model.LikeVideo, error) {
 	var like []*model.LikeVideo
 
-	err := r.DB.Model(&like).Where("video_id = ? && type = ?", videoid, typeArg).Select()
+	err := r.DB.Model(&like).Where("video_id = ? AND type = ?", videoid, typeArg).Select()
 
 	if err != nil {
 		return like, errors.New("video like / dislike not found!")
